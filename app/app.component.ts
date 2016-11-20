6a9cf557c18db13e2f624d5c4da8f9dacbc09fd1
James Carlson <jxxcarlson@gmail.com>
2016-11-20 14:00:45 -0500

    Display dummy list of documents + text of the first one

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,27 @@
 import { Component } from '@angular/core';
 
+import { Document } from './models/document'
+
 @Component({
     selector: 'my-app',
     templateUrl: './app/app.component.html',
     styleUrls: ['./app/app.component.css']
 })
-export class AppComponent {}
+export class AppComponent {
+
+    documents: Document[] = [
+        {
+            id: '1', authorId: '11',
+            title: 'Test',
+            text: 'This is a *test*',
+            renderedText: 'This is a <b>test</b>'
+        },
+        {
+            id: '2', authorId: '12',
+            title: 'Christmas',
+            text: 'Santa says _ho ho ho!_',
+            renderedText: 'Santa says <i>ho ho ho!</i>>'
+        }
+
+    ];
+}
