5dedb650280c1dba3edc8ccc9b4e9a2f5f1cd8e4
James Carlson <jxxcarlson@gmail.com>
2016-11-21 10:25:52 -0500

    x

--- app/reader/htmlpane.component.ts
+++ app/reader/htmlpane.component.ts
@@ -3,7 +3,7 @@ import { Document } from '../models/document';
 
 @Component({
     selector: 'html-pane',
-    template: '<div class="rendered-document" [innerHTML]=document.rendered_text></div>'
+    template: '<div class="rendered-document" [innerHTML]=document.rendered_text></div>',
     styles: [`.rendered-document {
     font-size: 2rem;
     height:1200px;
@@ -12,4 +12,5 @@ import { Document } from '../models/document';
 })
 export class HtmlPane {
     @Input() document:Document;
+
 }
