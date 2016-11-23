1ed5817dcafa79b9ef75ef8088bd73c82e055d21
James Carlson <jxxcarlson@gmail.com>
2016-11-22 20:28:13 -0500

    Remove unused code

--- app/directives/mathjax.directive.ts
+++ app/directives/mathjax.directive.ts
@@ -2,14 +2,6 @@
 import {Directive, ElementRef, Input} from 'angular2/core';
 
 
-
-// import { MathJax } from '../../node_modules/mathjax/MathJax.js'
-
-// import * as MJ from 'mathjax';
-
-// var MathJax = require('mathjax')
-//var MathJax = require('../../node_modules/mathjax/MathJax.js')
-
 declare var MathJax: any;
 
 import '../../node_modules/mathjax/MathJax.js'
@@ -22,22 +14,17 @@ export class MathJaxDirective {
 
     @Input('MathJax') texExpression:string;
 
-    // mathJaxObj: any;
-
     constructor(private el: ElementRef) {
 
-        // this.mathJaxObj = new MathJax("MathJax")
     }
 
     ngOnChanges() {
         this.el.nativeElement.innerHTML = this.texExpression;
         MathJax.Hub.Queue(["Typeset", MathJax.Hub, this.el.nativeElement]);
-        // this.mathJaxObj.Hub.Queue(["Typeset", this.mathJaxObj.Hub, this.el.nativeElement]);
     }
 }
 
 // EXTERNAL JS code: http://x-team.com/2016/06/include-javascript-libraries-in-an-ionic-2-typescript-project/
-
 
 // EXTERNAN JS code: https://github.com/AngularClass/angular2-webpack-starter/issues/570
 // EXTERNAL JS code: https://medium.com/@s_eschweiler/using-external-libraries-with-angular-2-87e06db8e5d1#.nb6kd65cu
@@ -46,4 +33,4 @@ export class MathJaxDirective {
 
 // SOURCE: http://stackoverflow.com/questions/36370826/how-to-get-mathjax-working-with-angular2
 
-// ANOTTHER SOURCE: http://ruinshe.moe/2016/05/31/support-mathjax-in-angular2/
+// ANOTHER SOURCE: http://ruinshe.moe/2016/05/31/support-mathjax-in-angular2/
--- app/reader/mathjaxpane.component.ts
+++ app/reader/mathjaxpane.component.ts
@@ -1,6 +1,5 @@
 import { Component, Input } from '@angular/core';
 import { Document } from '../models/document';
-import { MathJaxDirective } from  '../directives/mathjax.directive'
 
 @Component({
     selector: 'mathjax-pane',
